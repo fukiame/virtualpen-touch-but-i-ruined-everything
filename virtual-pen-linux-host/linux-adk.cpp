5b3e99d9a38f3ecd1d66cbe21642ae5c0fea76b1
fukiame <fukiame@proton.me>
2024-07-20 10:05:27 +0700

    treewide: rebrand accessory
    to not conflict with upstream
    not trying to replace upstream per se so

--- virtual-pen-linux-host/linux-adk.cpp
+++ virtual-pen-linux-host/linux-adk.cpp
@@ -34,12 +34,12 @@ volatile int stop_acc = 0;
 int verbose = 0;
 
 std::string device = "18d1:4ee2";
-std::string manufacturer = "mbeliveau";
-std::string model = "VirtualStylusHost";
-std::string description = "Linux Virtual Stylus Host";
+std::string manufacturer = "fukiame";
+std::string model = "VTabletHost";
+std::string description = "Virtual Tablet Host";
 std::string version = "1.0";
-std::string url = "https://github.com/Mathieu-Beliveau/android-pen-emulator";
-std::string serial = "0000000012345678";
+std::string url = "https://github.com/fukiame/virtualpen-touch-but-i-ruined-everything";
+std::string serial = "0000000072769420";
 
 static const accessory_t acc_default = {
     .device = device.data(),
