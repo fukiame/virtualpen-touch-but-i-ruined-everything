4cfb4e28e9132e1bcd187409838ae7e5a7b5b453
fukiame <fukiame@proton.me>
2024-07-20 09:42:19 +0700

    android: do all the fullscreen and landscape stuff on UsbConnectedActivity also

--- android-virtual-pen/app/src/main/java/com/virtual_pen/UsbConnectedActivity.java
+++ android-virtual-pen/app/src/main/java/com/virtual_pen/UsbConnectedActivity.java
@@ -29,6 +29,7 @@ public class UsbConnectedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_main);
         mainView = findViewById(R.id.main);
         ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
