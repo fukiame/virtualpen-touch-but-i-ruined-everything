b1df63225c7eba80000e575714e4aaa213ba5b6f
fukiame <fukiame@proton.me>
2024-07-20 13:37:27 +0700

    host: change config location

--- virtual-pen-linux-host/mainwindow.h
+++ virtual-pen-linux-host/mainwindow.h
@@ -49,7 +49,7 @@ private slots:
     void on_connectUsbButton_2_clicked();
 
 private:
-    const QString setting_org = "com.github.androidvirtualpen";
+    const QString setting_org = "com.github.fukiame";
     const QString setting_app = "virtualpen";
     const string y_device_setting_key = "/y_size";
     const string x_device_setting_key = "/x_size";
